b4b7a8ec2b0f084c6585b04307b0e16d5b3ff291
sara charafi <elkbircharafi8@gmail.com>
2026-08-12 16:56:55 +0100

    feat: add user tickets API

--- database/migrations/2026_08_10_144241_create_bookings_table.php
+++ database/migrations/2026_08_10_144241_create_bookings_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
             $table->foreignId('user_id')
                 ->constrained('users')
                 ->onDelete('cascade');
-
             $table->foreignId('event_id')
                 ->constrained('events')
                 ->onDelete('cascade');
