f33146bd6d70896a167305e84b402a790d4a9b7d
sara charafi <elkbircharafi8@gmail.com>
2026-08-13 12:46:21 +0100

    chore: organize src structure

--- BDE-Event-frontend/src/App.jsx
+++ BDE-Event-frontend/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
